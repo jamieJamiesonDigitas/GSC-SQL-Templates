0830e3b6b5a8f528a14060eaaeb01099791555cd
jamieJamiesonDigitas <86303153+jamieJamiesonDigitas@users.noreply.github.com>
2023-03-06 11:14:02 +0000

    Update ranking_buckets.sql

--- ranking_buckets.sql
+++ ranking_buckets.sql
@@ -1,14 +1,30 @@
 #Calculate top ranking buckets
 
 CASE
-  when round(position,0) < 11 Then 1
-  else 0
-END AS Top10,
-CASE
   when round(position,0) < 4 Then 1
   else 0
 END AS Top3,
 CASE
   when round(position,0) < 11 and round(position,0) > 4 Then 1
   else 0
-END AS Top4_10
+END AS Top4_10,
+CASE
+  when round(position,0) < 11 Then 1
+  else 0
+END AS Top10,
+CASE
+  when round(position,0) < 21 Then 1
+  else 0
+END AS Top20,
+CASE
+  when round(position,0) < 31 Then 1
+  else 0
+END AS Top30,
+CASE
+  when round(position,0) < 51 Then 1
+  else 0
+END AS Top50,
+CASE
+  when round(position,0) < 100 Then 1
+  else 0
+END AS Ranked
