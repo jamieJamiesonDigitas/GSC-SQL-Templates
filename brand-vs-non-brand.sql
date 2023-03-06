205f89b51a2930f5b8a1ea16dec6570b04ad4ace
jamieJamiesonDigitas <86303153+jamieJamiesonDigitas@users.noreply.github.com>
2023-03-06 11:12:29 +0000

    Update brand-vs-non-brand.sql

--- brand-vs-non-brand.sql
+++ brand-vs-non-brand.sql
@@ -1,6 +1,8 @@
+#create column based on query type (branded vs non-brand)
+
 SELECT date, page, device, query, country, clicks, impressions, position,
   CASE 
-    WHEN text_contains(query, r'[YOUR BRAND NAME') THEN 'Brand'
+    WHEN regex_contains(query, r'[YOUR BRAND NAME') THEN 'Brand'
     ELSE 'Non-brand'
   END AS queryType
 FROM ["DATASET NAME"]
